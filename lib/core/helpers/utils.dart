1cc822671d9d20a61115829b682e96976a57bb83
lilatee <marcin.hradowicz@gmail.com>
2025-01-07 21:41:16 +0100

    fix: openUrl

--- lib/core/helpers/utils.dart
+++ lib/core/helpers/utils.dart
@@ -3,12 +3,12 @@ import 'package:github_repo_finder/core/services/crashlytics_error_reporter.dart
 import 'package:url_launcher/url_launcher_string.dart';
 
 Future<void> openUrl(String url) async {
-  if (await canLaunchUrlString(url)) {
+  try {
     await launchUrlString(url);
-  } else {
+  } catch (err, st) {
     await sl<CrashlyticsErrorReporter>().logException(
-      exception: Exception('Cannot launch url: $url'),
-      stackTrace: StackTrace.current,
+      exception: Exception('Cannot launch url: $url. Error: $err'),
+      stackTrace: st,
     );
   }
 }
